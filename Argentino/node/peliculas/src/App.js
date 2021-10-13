70f6a4868188efc924d772ba61e95915314b6972
anfisbena <republicacoc@gmail.com>
2021-10-12 19:38:20 -0500

    main

--- Argentino/node/peliculas/src/App.js
+++ Argentino/node/peliculas/src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Paginacion from './Paginacion';
+import PageWrapper from './pageWrapp';
 import Pelicula from './pelicula';
-import pageWrapp from './pageWrapp';
+import peliculasJson from './Peliculas.json';
 
-export default function App() {
-	return(
-		<pageWrapp/>
-		
-	)
+function App() {
 
+  const [paginaActual, setPaginaActual] = useState(1);
+  const TOTAL_POR_PAGINA = 7;
+
+  let peliculas = peliculasJson;
+
+  const cargarPeliculas = () => {
+    peliculas = peliculas.slice(
+      (paginaActual - 1) * TOTAL_POR_PAGINA,
+      paginaActual * TOTAL_POR_PAGINA
+    );
+  }
+
+  const getTotalPaginas = () => {
+    let cantidadTotalDePeliculas = peliculasJson.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+  cargarPeliculas();
+
+  return (
+    <PageWrapper>
+
+      {peliculas.map(pelicula =>
+        <Pelicula 
+			titulo={pelicula.titulo} 
+			calificacion={pelicula.calificacion}
+          	director={pelicula.director} 
+			actores={pelicula.actores} 
+			fecha={pelicula.fecha} 
+			duracion={pelicula.duracion}
+          	img={pelicula.img}>
+          	{pelicula.descripcion}
+        </Pelicula>)}
+
+      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+        setPaginaActual(pagina)
+      }} />
+
+    </PageWrapper>
+  );
 }
+
+export default App;
--- Argentino/node/peliculas/src/pelicula.js
+++ Argentino/node/peliculas/src/pelicula.js
@@ -1,7 +1,7 @@
 export default function Pelicula(props){
     return(
         <div className="movie-item-style-2">
-			<img src={props.imagen} alt="" />
+			<img src={props.img} alt="" />
 			<div className="mv-item-infor">
 				<h6><a href="moviesingle.html">{props.titulo}</a></h6>
                 <p className="rate"><i className="ion-android-star"></i><span>{props.calificacion}</span> /10</p>
